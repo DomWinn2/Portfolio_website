0a3ed36317fd1de4315e884e5466e7d5ff882e5c
Dominick Winningham <dominickwinningham@gmail.com>
2025-06-25 23:36:11 -0400

    Able to see background for light mode now

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,34 +9,34 @@ import { ContactSection } from "../components/ContactSection";
 import { Footer } from "../components/Footer";
 
 export const Home = () => {
-    return <div className="min-h-screen bg-background text-foreground overflow-y-scroll snap-y snap-mandatory scroll-smooth overflow-x-hidden">
-
-        {/* Theme Toggle */}
-        <ThemeToggle />
-        {/* Background Effects */}
-        <StarBackground />
-        {/* Navbar */}
-        <Navbar />
-        {/* Main Content */}
-        <main>
-            <section className="snap-start h-screen">
-                <HeroSection />
-            </section>
-            <section className="snap-start h-screen">
-                <AboutMeSection />
-            </section>
-            <section className="snap-start h-screen">
-                <SkillsSection />
-            </section>
-            <section className="snap-center h-screen">
-                <ProjectSection />
-            </section>
-            <section className="snap-center h-screen">
-                <ContactSection />
-            </section>
-        </main>
-        {/* Footer */}
-        <Footer />
-
-    </div>;
+    return (
+        <div className="min-h-screen bg-background text-foreground overflow-y-scroll snap-y snap-mandatory scroll-smooth overflow-x-hidden">
+            {/* Theme Toggle */}
+            <ThemeToggle />
+            {/* Background Effects */}
+            <StarBackground />
+                {/* Navbar */}
+                <Navbar />
+                {/* Main Content */}
+                <main>
+                    <section className="snap-start h-screen">
+                        <HeroSection />
+                    </section>
+                    <section className="snap-start h-screen">
+                        <AboutMeSection />
+                    </section>
+                    <section className="snap-start h-screen">
+                        <SkillsSection />
+                    </section>
+                    <section className="snap-center h-screen">
+                        <ProjectSection />
+                    </section>
+                    <section className="snap-center h-screen">
+                        <ContactSection />
+                    </section>
+                </main>
+                {/* Footer */}
+                <Footer />
+        </div>
+    );
 };
